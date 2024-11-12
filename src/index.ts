200775ebbdbde78ffdd017730a4ffc05f46b859a
Anastasiia Sliusar <anastasiia.sliusar@gmail.com>
2024-11-12 17:06:07 +0100

    Fix parsing files and saving them

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,43 @@
-
 import { IFileData } from './types';
-import untarjs from "@emscripten-forge/untarjs";
+import untarjs from '@emscripten-forge/untarjs';
 
 const checkConda = (url: string): boolean => {
   let flag = false;
-  if (url.toLowerCase().endsWith(".conda")) {
+  if (url.toLowerCase().endsWith('.conda')) {
     flag = true;
   }
   return flag;
-}
+};
 
-const installCondaPackage = async (prefix: string, url: string, FS: any): Promise<void> => {
+const installCondaPackage = async (
+  prefix: string,
+  url: string,
+  FS: any
+): Promise<void> => {
   let files: IFileData[] = await untarjs.extract(url);
   const isCondaPackage = checkConda(url);
-  if(isCondaPackage) {
-    console.log(
-      `!!extract conda package ${url})`
-    );
+  if (isCondaPackage) {
+    console.log(`!!extract conda package ${url})`);
 
-    let condaPackage = files.filter((file) => {
-      if (file.filename.startsWith("pkg-")) {
-        console.log("This is a package file.");
+    let condaPackage = files.filter(file => {
+      if (file.filename.startsWith('pkg-')) {
+        console.log('This is a package file.');
         return file;
       }
     });
-    condaPackage.map(async (pkg) => {
-        const condaFiles: IFileData[] = await untarjs.extractData(pkg.data);
-        saveFiles(prefix, FS, condaFiles);
+    condaPackage.map(async pkg => {
+      const condaFiles: IFileData[] = await untarjs.extractData(pkg.data);
+      saveFiles(prefix, FS, condaFiles);
     });
   } else {
     saveFiles(prefix, FS, files);
   }
-}
+};
 
 const saveFiles = (prefix: string, FS: any, files: IFileData[]): void => {
   try {
-    let filteredFilesPkg = files.filter((file) => {
-      let regexp = /^(.*\/)site-packages\//;
+    let filteredFilesPkg = files.filter(file => {
+      let regexp = 'site-packages';
       if (file.filename.match(regexp)) {
         return file;
       }
@@ -50,32 +51,39 @@ const saveFiles = (prefix: string, FS: any, files: IFileData[]): void => {
       FS.mkdirTree(destDir);
     }
 
-    filteredFilesPkg.map((file) => {
+    filteredFilesPkg.map(file => {
       writeFile(file.data, file.filename, FS, 'site-packages', destDir);
     });
 
-    ["etc", "share"].forEach(folder => {
+    ['etc', 'share'].forEach(folder => {
       let folderDest = `${prefix}/${folder}`;
-      files.map((file) => {
-        let regexp = `^(.*\/)${folder}\/`;
-        if(file.filename.match(regexp)) {
+
+      files.map(file => {
+        let regexp = `${folder}`;
+        if (file.filename.match(regexp)) {
+          console.log('files for etc and share', file.filename);
           writeFile(file.data, file.filename, FS, folder, folderDest);
         }
       });
     });
-
   } catch (e) {
-    console.error("Error during saving", e);
+    console.error('ERROR', e);
     throw e;
   }
-}
+};
 
-const writeFile = (data: Uint8Array, filename: string, FS: any, folder: string, folderDest: string): void => {
+const writeFile = (
+  data: Uint8Array,
+  filename: string,
+  FS: any,
+  folder: string,
+  folderDest: string
+): void => {
   let regexp = `${folder}`;
   if (filename.match(regexp)) {
     const regexp = `${folder}\/(.+?)\/[^\/]+$`;
 
-    let directoryPathes = getDirectoryPathes(filename, regexp)
+    let directoryPathes = getDirectoryPathes(filename, regexp);
     console.log(directoryPathes);
     let destPath = `${folderDest}/${directoryPathes}`;
     if (destPath) {
@@ -89,16 +97,16 @@ const writeFile = (data: Uint8Array, filename: string, FS: any, folder: string, 
 
     destPath = `${destPath}${fileName}`;
 
-    FS.writeFile(destPath, data, { encoding: 'binary' });
+    let encodedData = new TextDecoder('utf-8').decode(data);
+    FS.writeFile(destPath, encodedData);
   }
-}
+};
 
 const getDirectoryPathes = (filename: string, regexp: any): string => {
   let match = filename.match(regexp);
   let directoryPathes = match ? match[1] : '';
   return directoryPathes;
-}
-
+};
 
 export default {
   installCondaPackage
