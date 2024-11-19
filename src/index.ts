f5fd91a990a6f65e956b4c938205fb65ae420e16
Anastasiia Sliusar <anastasiia.sliusar@gmail.com>
2024-11-19 15:50:45 +0100

    Fix changes of saving files

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
-import { extract, IFileData, extractData } from '@emscripten-forge/untarjs';
+import { extract, extractData, IFileData } from '@emscripten-forge/untarjs';
 
 export const installCondaPackage = async (
   prefix: string,
   url: string,
   FS: any,
-  verbose: false
+  verbose: boolean
 ): Promise<void> => {
   try {
     let files: IFileData[] = await extract(url);
     if (files.length !== 0) {
-      console.log('exctracted files', files);
       if (url.toLowerCase().endsWith('.conda')) {
         let condaPackage: IFileData = {
           filename: '',
@@ -28,16 +27,15 @@ export const installCondaPackage = async (
             packageInfo = file;
           }
         });
-
         const condaFiles: IFileData[] = await extractData(condaPackage.data);
         const packageInfoFiles: IFileData[] = await extractData(
           packageInfo.data
         );
-        createCondaMetaFile(packageInfoFiles, prefix, FS);
+        createCondaMetaFile(packageInfoFiles, prefix, FS, verbose);
         let mergedFiles = [...condaFiles, ...packageInfoFiles];
         saveFiles(prefix, FS, mergedFiles, verbose);
       } else {
-        createCondaMetaFile(files, prefix, FS);
+        createCondaMetaFile(files, prefix, FS, verbose);
         saveFiles(prefix, FS, files, verbose);
       }
     } else {
@@ -52,77 +50,86 @@ const saveFiles = (
   prefix: string,
   FS: any,
   files: IFileData[],
-  verbose: false
+  verbose: boolean
 ): void => {
+  console.log('Saving files into browser memory');
+
   try {
-    ['info', 'site-packages', 'etc', 'share'].forEach(folder => {
+    ['site-packages', 'info', 'etc', 'share'].forEach(folder => {
       let folderDest = `${prefix}/${folder}`;
-      if ((folder = 'site-packages')) {
+      if (folder === 'site-packages') {
         folderDest = `${prefix}/lib/python3.11/site-packages`;
       }
-
-      files.map(file => {
-        let regexp = `${folder}`;
-        if (file.filename.match(regexp)) {
-          if (!FS.analyzePath(folderDest).exists) {
-            FS.mkdirTree(folderDest);
-          }
-          console.log(`files for ${folderDest} folder`, file.filename);
-          writeFile(file.data, file.filename, FS, folder, folderDest, verbose);
-        }
-      });
+      savingFiles(files, folder, folderDest, FS, verbose);
     });
   } catch (error) {
     console.error(error);
   }
 };
 
+const savingFiles = (
+  files: IFileData[],
+  folder: string,
+  folderDest: string,
+  FS: any,
+  verbose: boolean
+) => {
+  files.forEach(file => {
+    const regexp = new RegExp(`^${folder}`);
+    if (file.filename.match(regexp)) {
+      if (!FS.analyzePath(folderDest).exists) {
+        FS.mkdirTree(folderDest);
+      }
+      if (verbose) {
+        console.log(`Writing a file for ${folderDest} folder`, file.filename);
+      }
+      writeFile(file.data, file.filename, FS, folder, folderDest, verbose);
+    }
+  });
+};
+
 const writeFile = (
   data: Uint8Array,
-  filename: string,
+  fullPath: string,
   FS: any,
   folder: string,
   folderDest: string,
-  verbose: false
+  verbose: boolean
 ): void => {
-  let regexp = `${folder}`;
-  if (filename.match(regexp)) {
-    const regexp = `${folder}\/(.+?)\/[^\/]+$`;
+  let fileName = fullPath.substring(fullPath.lastIndexOf('/') + 1);
 
-    let directoryPathes = getDirectoryPathes(filename, regexp);
-    console.log(directoryPathes);
-    let destPath = `${folderDest}/${directoryPathes}`;
-    if (destPath) {
-      if (!FS.analyzePath(destPath).exists) {
-        FS.mkdirTree(destPath);
-      }
-    }
-
-    let fileName = filename.substring(filename.lastIndexOf('/') + 1);
-    console.log('fileName', fileName);
-
-    destPath = `${destPath}${fileName}`;
-
-    if (verbose) {
-      console.log(`Saving files into ${destPath}`);
-    }
-
-    let encodedData = new TextDecoder('utf-8').decode(data);
-    FS.writeFile(destPath, encodedData);
+  let directoryPathes = fullPath.replace(new RegExp(`\/${fileName}`), '');
+  if (directoryPathes.match(folder)) {
+    directoryPathes = directoryPathes.replace(new RegExp(`${folder}`), '');
   }
+
+  let destPath = `${folderDest}${directoryPathes}/`;
+  if (destPath) {
+    if (!FS.analyzePath(destPath).exists) {
+      FS.mkdirTree(destPath);
+    }
+  }
+
+  destPath = `${destPath}${fileName}`;
+
+  if (verbose) {
+    console.log(`Saving files into ${destPath}`);
+  }
+
+  let encodedData = new TextDecoder('utf-8').decode(data);
+  FS.writeFile(destPath, encodedData);
 };
 
-const getDirectoryPathes = (filename: string, regexp: any): string => {
-  let match = filename.match(regexp);
-  let directoryPathes = match ? match[1] : '';
-  return directoryPathes;
-};
-
-const createCondaMetaFile = (files: IFileData[], prefix: string, FS: any) => {
+const createCondaMetaFile = (
+  files: IFileData[],
+  prefix: string,
+  FS: any,
+  verbose: boolean
+) => {
   let infoData: Uint8Array = new Uint8Array();
 
   files.map((file: IFileData) => {
-    let regexp = 'info.json';
+    let regexp = 'index.json';
 
     if (file.filename.match(regexp)) {
       infoData = file.data;
@@ -143,9 +150,14 @@ const createCondaMetaFile = (files: IFileData[], prefix: string, FS: any) => {
       };
 
       if (!FS.analyzePath(`${condaMetaDir}`).exists) {
-        FS.mkdirTree(`${prefix}/conda-meta`);
+        FS.mkdirTree(`${condaMetaDir}`);
       }
 
+      if (verbose) {
+        console.log(
+          `Creating conda-meta file for ${condaPackageInfo.name}-${condaPackageInfo.version}-${condaPackageInfo.build} package`
+        );
+      }
       FS.writeFile(path, JSON.stringify(pkgCondaMeta));
     } catch (error) {
       console.error(error);
